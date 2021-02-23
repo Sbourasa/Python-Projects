184878e1219dfd5e24f6bda001e439a3e3a029db
sbourasa <shanebourasa@gmail.com>
2021-02-22 20:42:13 -0600

    added a few more examples

--- example3.py
+++ example3.py
@@ -1,4 +1,4 @@
 #Python Program to Find the Square Root
-num = 8
-
-print(num**0.5)
+num = input("Enter a number: ")
+answer = (float(num) ** 0.5) #raise to the power of 1/2 (sqrt)
+print(answer)
---
+++ example4.py
@@ -0,0 +1,3 @@
+#Python Program to Calculate the Area of a Triangle
+
+#formula for calculating the area of a triangle 
